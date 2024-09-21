1f7f3e8e9ded8c23daaa311fa857ecb15119d4cc
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-21 22:05:12 +0600

    module overview of dom

---
+++ js/first.js
@@ -0,0 +1,14 @@
+console.log("first file");
+const allLi = document.getElementsByTagName("li");
+console.log(allLi);
+
+const title = document.getElementsByClassName("section-title");
+console.log(title);
+
+const secondSection = document.getElementById("second-section");
+console.log(secondSection);
+secondSection.style.color = 'violet'
+secondSection.style.backgroundColor = 'lightgrey'
+
+// document.querySelectorAll
+// document.querySelector
