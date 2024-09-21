7d21bd085c6a95a720761e47518ee93a31fa2e26
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-21 19:52:16 +0600

    append example in js.

--- js/append.js
+++ js/append.js
@@ -7,3 +7,29 @@ const li = document.createElement("li");
 li.innerText = "pahartoli bon";
 // 3. add the child
 placesList.appendChild(li);
+
+// 1. where to add
+const mainContainer = document.getElementById("main-container");
+// console.log(mainContainer);
+// 2. what to be added
+const section = document.createElement("section");
+const h1 = document.createElement("h1");
+h1.innerText = "My Food List";
+section.appendChild(h1);
+
+// creating list item
+const ul = document.createElement('ul');
+const li1 = document.createElement("li");
+li1.innerText = "biriyani";
+ul.appendChild(li1);
+
+const li2 = document.createElement("li");
+li2.innerText = "kacchi";
+ul.appendChild(li2);
+
+const li3 = document.createElement("li");
+li3.innerText = "borhani";
+ul.appendChild(li3);
+
+section.appendChild(ul);
+mainContainer.appendChild(section);
